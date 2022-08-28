54a08e0c5ad719ddc0e80a8fd908dbd3185417cf
luk93 <ligajlukasz93@gmail.com>
2022-08-28 15:28:18 +0200

    Add project files.

---
+++ Db/MyDbContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TextFileExport.Db
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AlarmsTrms001> AlarmsTrms001s { get; set; } = null!;
+        public virtual DbSet<MessagesTrms001> MessagesTrms001s { get; set; } = null!;
+        public virtual DbSet<WarningsTrms001> WarningsTrms001s { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source = localhost\\SQLEXPRESS; Database = CPM; User ID = root; Password = root; Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AlarmsTrms001>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Alarms_TRMS001");
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<MessagesTrms001>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Messages_TRMS001");
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<WarningsTrms001>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Warnings_TRMS001");
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Db/WarningsTrms001.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextFileExport.Db
+{
+    public partial class WarningsTrms001
+    {
+        public int Id { get; set; }
+        public int IdAlarm { get; set; }
+        public string? Comment { get; set; }
+    }
+}
